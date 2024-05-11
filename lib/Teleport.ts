598ccdaf703acbdecb79e85e5708488b369f2ef6
jackhuang <2504448153@qq.com>
2024-05-11 11:53:45 +0800

    fix: teleport being directly inserted into another teleport

--- lib/Teleport.ts
+++ lib/Teleport.ts
@@ -2,9 +2,14 @@ import { defineComponent, CreateComponentPublicInstance } from 'vue';
 
 type BaseData = {
     comment: Comment;
+    isV2TPN: true;
 };
-
-type TeleportInstance = CreateComponentPublicInstance<unknown, unknown, BaseData>;
+type TeleportNode = {
+    check: () => void;
+    transfer: () => void;
+    revert: () => void;
+};
+type TeleportInstance = CreateComponentPublicInstance<unknown, unknown, BaseData> & TeleportNode;
 
 const getComponentChildrenNode = (context: TeleportInstance): HTMLElement[] => {
     // @ts-expect-error must has children
@@ -16,7 +21,18 @@ const getFragment = (context: TeleportInstance): DocumentFragment => {
     // See https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment
     // Inspire by https://github.com/Mechazawa/vue2-teleport
     const fragment = document.createDocumentFragment();
-    getComponentChildrenNode(context).forEach((node) => fragment.appendChild(node));
+    const teleportNodes: TeleportInstance[] = [];
+    getComponentChildrenNode(context)
+        .map((ele) => {
+            // @ts-expect-error teleport
+            const vm: TeleportInstance | undefined = ele.__vue__;
+            if (!vm || !vm.isV2TPN) return ele;
+            teleportNodes.push(vm);
+            return vm.comment;
+        })
+        .forEach((node) => fragment.appendChild(node));
+    // Resolve the issue of teleport being directly inserted into another teleport
+    teleportNodes.forEach((teleport) => teleport.check());
     return fragment;
 };
 
@@ -33,9 +49,7 @@ const Teleport = defineComponent({
                 // Inspire by https://stackoverflow.com/questions/47511674/a-way-to-render-multiple-root-elements-on-vuejs-with-v-for-directive
                 const componentInstance = vnode.context as unknown as TeleportInstance;
                 if (!componentInstance) return;
-                getFragment(componentInstance);
                 const parent = el.parentElement;
-                // TODO Should resolve direct parent is fragment node.
                 parent && parent.replaceChild(componentInstance.comment, el);
             },
         },
@@ -43,6 +57,7 @@ const Teleport = defineComponent({
     data() {
         const base = {
             comment: document.createComment('Teleport'),
+            isV2TPN: true,
         } as BaseData;
         return Object.preventExtensions(base);
     },
@@ -52,8 +67,9 @@ const Teleport = defineComponent({
     updated() {
         this.check();
     },
-    beforeUnmount() {
-        getFragment(this);
+    // eslint-disable-next-line vue/no-deprecated-destroyed-lifecycle
+    beforeDestroy() {
+        this.revert();
     },
     render(h) {
         return h('span', { directives: [{ name: 'fragments' }] }, this.$slots.default);
@@ -64,6 +80,7 @@ const Teleport = defineComponent({
         },
         revert() {
             // Insert slot children before comment node
+            // TODO No need to re-invoke if already inserted.
             this.comment.parentNode?.insertBefore(getFragment(this), this.comment);
         },
         transfer() {
