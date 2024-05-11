1372dacde2f6c31ac2859bcc8c38a4606737045f
jackhuang <2504448153@qq.com>
2024-05-11 09:16:17 +0800

    refactor: simplify logic and reduce exposed attributes.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
 <template>
     <div>
         <button @click="onToggleDisable">{{ disabled ? 'Enable' : 'Disable' }}</button>
+        <button @click="onToggleDisable2">{{ disabled2 ? 'Enable2' : 'Disable2' }}</button>
         <button @click="onChangeTarget">move to {{ to }}</button>
-        <vue-teleport :to="to" :disabled="disabled">
+        <button @click="onChangeTarget2">move 2 to {{ to2 }}</button>
+        <button @click="to2 = 'body'">move 2 to body</button>
+        <button @click="to2 = '.target2'">move 2 to target2</button>
+        <Teleport :to="to" :disabled="disabled">
             <div>A</div>
-            <div>B</div>
-            <div>C</div>
-        </vue-teleport>
+            <transition>
+                <section class="ddd" v-show="true">
+                    222
+                    <Teleport :to="to2" :disabled="disabled2">
+                        <div>D</div>
+                    </Teleport>
+                    333
+                </section>
+            </transition>
+            <!-- <Teleport :to="to" :disabled="disabled">
+                <div>G</div>
+            </Teleport> -->
+        </Teleport>
         <h1>before</h1>
         <div class="target">target node</div>
         <h1>after</h1>
+        <div class="target2">target node</div>
+        <h1>1111</h1>
+
+        <button @click="modalOpen = true">Open full-screen modal! (With teleport!)</button>
+
+        <Teleport to="body">
+        <div v-show="modalOpen" class="modal">
+            <div v-if="modalOpen">
+            I'm a teleported modal! (My parent is "body")
+            <button @click="modalOpen = false">Close</button>
+            </div>
+        </div>
+        </Teleport>
     </div>
 </template>
 
 <script>
-import VueTeleport from '../lib/Teleport.vue';
+import Teleport from '../lib/Teleport';
 
 export default {
     components: {
-        VueTeleport,
+        Teleport,
     },
     data() {
         return {
             to: 'body', // selector of the target element
-            disabled: false,
+            to2: 'body',
+            disabled: true,
+            disabled2: true,
+            modalOpen: false,
         };
     },
     methods: {
@@ -32,6 +62,12 @@ export default {
         },
         onChangeTarget() {
           this.to = this.to === 'body' ? '.target' : 'body';
+        },
+        onToggleDisable2() {
+          this.disabled2 = !this.disabled2;
+        },
+        onChangeTarget2() {
+          this.to2 = this.to2 === 'body' ? '.target2' : 'body';
         }
     },
 };
